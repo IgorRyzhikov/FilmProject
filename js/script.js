bbaab836a6ee4d6c94b2ce430c64ac967b22666f
Igor <iryzhikov.igor@gmail.com>
2021-08-31 14:23:53 +0300

    second commit v1

--- js/script.js
+++ js/script.js
@@ -10,12 +10,29 @@ const personalMovieBD = {
     privat: false
 };
 
-const q1 = prompt("Один из последних просмотренных фильмов?", ""),
-      q2 = +prompt("На сколько оцените его?", ""),
-      q3 = prompt("Один из последних просмотренных фильмов?", ""),
-      q4 = +prompt("На сколько оцените его?", "");
+for (let i = 0; i < 2; i++) {
 
-personalMovieBD.movies[q1] = q2;
-personalMovieBD.movies[q3] = q4 ;
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+          b = +prompt("На сколько оцените его?", "");
+
+    if ( a != null && b != null && a != '' && b != '' && a.length < 50) {    
+        personalMovieBD.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('Error!');
+        i--;
+    }
+}
+
+if (personalMovieBD.count < 10) {
+    console.log("Просмотрено довольно мало фильмов!");
+} else if (personalMovieBD.count >= 10 && personalMovieBD.count < 30) {
+    console.log('Вы классический зритель!');
+} else if (personalMovieBD.count >= 30) {
+    console.log("Вы киноман!");
+} else {
+    console.log("Произошла ошибка!");
+}
+
 
 console.log(personalMovieBD);
